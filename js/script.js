b42e1db3cba2fccc0ce3b5de98d26cab1c63abb9
Fernanda Lowe <fe_lowe@hotmail.com>
2016-03-25 16:55:32 -0700

    add more results to user search

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".profile").submit(function(event){
-   event.preventDefault();
+    event.preventDefault();
 
     var countryInput = $ ("input:radio[name=country]:checked").val();
     var spotInput = $("input:radio[name=spot]:checked").val();
@@ -17,14 +17,31 @@ $(document).ready(function() {
     console.log(lastPlacesInput);
     console.log(bestPlacesInput);
 
-      if (countryInput === "overseas" && spotInput === "beach" && ageInput === "under40" && companyInput === "friend") {
-        $(".rio, .tahiti").show();
-      } else if (countryInput === "us" && spotInput === "beach" && ageInput === "under40" && companyInput === "family") {
-          $(".hawaii").show();
-      }
+    if (countryInput === "overseas" && spotInput === "beach" && ageInput === "under40" && companyInput === "friend") {
+      $(".rio, .tahiti").show();
 
-    });
-    //   else {
-    //     if
-    // }
+    } else if (countryInput === "us" && spotInput === "beach" && ageInput === "under40" && companyInput === "family") {
+      $(".hawaii").show();
+
+    } else if (countryInput === "us"){
+      $(".hawaii, .oregon, .aspen").show();
+
+    } else if (countryInput === "overseas"){
+      $(".rio, .tahiti, .greenland, .machu").show();
+
+    } else if (spotInput === "beach"){
+      $(".rio, .tahiti, .hawaii").show();
+
+    } else if (spotInput === "snow"){
+      $(".greenland, .aspen").show();
+
+    } else if (spotInput === "mountain"){
+      $(".oregon, .machu").show();
+
+    } else if (ageInput === "age"){
+      $(".allResults").show();
+    }
+
   });
+
+});
