18f6fda234c4b66a74c0462ec89f84a3893ee4d1
Fernanda Lowe <fe_lowe@hotmail.com>
2016-03-25 16:26:57 -0700

    show search results as userhit submit button

--- js/script.js
+++ js/script.js
@@ -1,35 +1,30 @@
 $(document).ready(function() {
-$("form#questions").submit(function(event){
- event.preventDefault();
+  $(".profile").submit(function(event){
+   event.preventDefault();
 
-  var countryInput = $("#country").val();
-  var spotInput = $("#spot").val();
-  var ageInput = $("#age").val();
-  var daysInput = $("#days").val();
-  var companyInput = $("#company").val();
-  var last3Input = $("#last3").val();
-  var bestPlacesInput = $("#bestPlaces").val();
-  console.log(countryInput);
-  console.log(spotInput);
-  console.log(ageInput);
-  console.log(daysInput);
-  console.log(companyInput);
-  console.log(last3Input);
-  console.log(bestPlacesInput);
+    var countryInput = $ ("input:radio[name=country]:checked").val();
+    var spotInput = $("input:radio[name=spot]:checked").val();
+    var ageInput = $("#age").val();
+    var daysInput = $("#days").val();
+    var companyInput = $("#company").val();
+    var lastPlacesInput = $("#lastPlaces").val();
+    var bestPlacesInput = $("#bestPlaces").val();
+    console.log(countryInput);
+    console.log(spotInput);
+    console.log(ageInput);
+    console.log(daysInput);
+    console.log(companyInput);
+    console.log(lastPlacesInput);
+    console.log(bestPlacesInput);
 
-    if (countryInput === "overseas" && spotInput === "beach" && ageInput === "under30" && companyInput === "friend") {
-      $(".rio").show();
-    // } else if (ageInput  ==="over50") {
-    //   $(".Obama, .John").show();
-    // } else if ( ageInput  ==="over40") {
-    //   $('.John').show();
-    // } else if (ageInput  ==="under30") {
-    //   $('.Matt').show();
+      if (countryInput === "overseas" && spotInput === "beach" && ageInput === "under40" && companyInput === "friend") {
+        $(".rio, .tahiti").show();
+      } else if (countryInput === "us" && spotInput === "beach" && ageInput === "under40" && companyInput === "family") {
+          $(".hawaii").show();
+      }
 
-    }
-  // }
-  //   else {
-  //     if
-  // }
+    });
+    //   else {
+    //     if
+    // }
   });
-});
